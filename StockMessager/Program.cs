e211f2af4e679d834a7d9c81f3a89db504049ee2
Atif Khokhar <atif.khokhar@asos.com>
2018-04-27 14:46:18 +0100

    first commit - created initial setup for ASB and stock item DTO object

--- StockMessager/Program.cs
+++ StockMessager/Program.cs
@@ -1,15 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Configuration;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
 
 namespace StockMessager
 {
     class Program
     {
+        string ServiceBusConnectionString = ConfigurationManager.ConnectionStrings["StockEventServiceBus"].ToString();
+        const string TopicName = "AtifStockEvent";
+        static ITopicClient topicClient;
+
         static void Main(string[] args)
         {
+
         }
     }
 }
---
+++ StockMessager/StockItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockMessager
+{
+    public class StockItem
+    {
+        public string Sku { get; set; }
+        public int Quantity { get; set; }
+        public string Warehouse { get; set; }
+        public string DeliveryProvider { get; set; }
+    }
+}
